c3f93b856616ead86bfd734d1aa1e8d094794071
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-08-21 22:16:08 -0300

    feat: adicionar os endpoints

--- src/main/java/com/rlevi/encurtador_url/controller/UrlController.java
+++ src/main/java/com/rlevi/encurtador_url/controller/UrlController.java
@@ -1,32 +1,40 @@
 package com.rlevi.encurtador_url.controller;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.rlevi.encurtador_url.service.UrlService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/")
 public class UrlController {
-    @Autowired
-    private UrlService service;
+    private final UrlService service;
 
     public UrlController(UrlService service) {
         this.service = service;
     }
 
     @PostMapping()
-    public ResponseEntity<Map<String, String>> shotUrl(@RequestBody Map<String, String> body) {
+    public ResponseEntity<Map<String, String>> shortUrl(@RequestBody Map<String, String> body) {
+        String originalUrl = body.get("url");
         String shortUrl = service.shotUrl(originalUrl);
-        Map<String, String> response = new HashMap<>();
-        response.put("shortUrl", shortUrl);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(Map.of("url", "http://localhost:8080/" + shortUrl));
+    }
+
+    @GetMapping("/{shortUrl}")
+    public Object redirect(@PathVariable String shortUrl) {
+        String originalUrl = service.getOriginalUrl(shortUrl);
+        if (originalUrl == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return new RedirectView(originalUrl);
     }
 }
