a924c5a037c0b4688f71ed49c53132e85734147b
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-08-21 21:54:22 -0300

    fix: adicionar linhas ao modelo

--- src/main/java/com/rlevi/encurtador_url/model/Url.java
+++ src/main/java/com/rlevi/encurtador_url/model/Url.java
@@ -3,14 +3,22 @@ package com.rlevi.encurtador_url.model;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Url {
 
     @Id
