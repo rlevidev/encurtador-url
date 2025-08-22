5e7545e6c1fc2b8e34d36ace52f26a7a74d62e4c
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-08-21 21:40:40 -0300

    first commit

---
+++ src/test/java/com/rlevi/encurtador_url/EncurtadorUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlevi.encurtador_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
